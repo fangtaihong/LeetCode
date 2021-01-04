d8ccc091aa1e72b22204fdf43d76349d8684b545
方太鸿\ST Cloud QA Part <fang_taihong@nhn-st.com>
2021-01-04 17:51:45 +0900

    add speed test

--- leetcood/src/test/java/leetcood/sort/SortTest.java
+++ leetcood/src/test/java/leetcood/sort/SortTest.java
@@ -2,16 +2,26 @@ package leetcood.sort;
 
 import java.util.Arrays;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class SortTest {
 
-	int[] arr = { 5, 6, 3, 1, 8, 7, 2, 4 };
+	int[] arr = { 5, 6, 3, 1, 8, 7, 2, 4, 12, 45, 89, 78, 457, 12, 48 };
+	long begin = 0;
+
+	@Before
+	public void be() {
+		// 速度测试
+		begin = System.currentTimeMillis();
+	}
 
 	@Test
 	public void bubbleSort() throws Exception {
 		BubbleSort sort = new BubbleSort();
 		int[] newArr = sort.sort(arr);
+		long end = System.currentTimeMillis();
+		System.out.println("bubbleSort用时：" + (end - begin));
 		System.out.println(Arrays.toString(newArr));
 	}
 
@@ -19,6 +29,8 @@ public class SortTest {
 	public void selectionSort() throws Exception {
 		SelectionSort sort = new SelectionSort();
 		int[] newArr = sort.sort(arr);
+		long end = System.currentTimeMillis();
+		System.out.println("selectionSort用时：" + (end - begin));
 		System.out.println(Arrays.toString(newArr));
 	}
 
@@ -26,6 +38,8 @@ public class SortTest {
 	public void insertSort() throws Exception {
 		InsertSort sort = new InsertSort();
 		int[] newArr = sort.sort(arr);
+		long end = System.currentTimeMillis();
+		System.out.println("insertSort用时：" + (end - begin));
 		System.out.println(Arrays.toString(newArr));
 	}
 
@@ -33,6 +47,8 @@ public class SortTest {
 	public void shellSort() throws Exception {
 		ShellSort sort = new ShellSort();
 		int[] newArr = sort.sort(arr);
+		long end = System.currentTimeMillis();
+		System.out.println("shellSort用时：" + (end - begin));
 		System.out.println(Arrays.toString(newArr));
 	}
 
@@ -40,6 +56,8 @@ public class SortTest {
 	public void mergeSort() throws Exception {
 		MergeSort sort = new MergeSort();
 		int[] newArr = sort.sort(arr);
+		long end = System.currentTimeMillis();
+		System.out.println("mergeSort用时：" + (end - begin));
 		System.out.println(Arrays.toString(newArr));
 	}
 
@@ -47,6 +65,8 @@ public class SortTest {
 	public void quickSort() throws Exception {
 		QuickSort sort = new QuickSort();
 		int[] newArr = sort.sort(arr);
+		long end = System.currentTimeMillis();
+		System.out.println("quickSort用时：" + (end - begin));
 		System.out.println(Arrays.toString(newArr));
 	}
 
@@ -54,6 +74,8 @@ public class SortTest {
 	public void heapSort() throws Exception {
 		HeapSort sort = new HeapSort();
 		int[] newArr = sort.sort(arr);
+		long end = System.currentTimeMillis();
+		System.out.println("heapSort用时：" + (end - begin));
 		System.out.println(Arrays.toString(newArr));
 	}
 
@@ -61,6 +83,8 @@ public class SortTest {
 	public void countingSort() throws Exception {
 		CountingSort sort = new CountingSort();
 		int[] newArr = sort.sort(arr);
+		long end = System.currentTimeMillis();
+		System.out.println("countingSort用时：" + (end - begin));
 		System.out.println(Arrays.toString(newArr));
 	}
 
@@ -68,6 +92,8 @@ public class SortTest {
 	public void BucketSort() throws Exception {
 		BucketSort sort = new BucketSort();
 		int[] newArr = sort.sort(arr);
+		long end = System.currentTimeMillis();
+		System.out.println("BucketSort用时：" + (end - begin));
 		System.out.println(Arrays.toString(newArr));
 	}
 
@@ -75,6 +101,8 @@ public class SortTest {
 	public void radixSort() throws Exception {
 		RadixSort sort = new RadixSort();
 		int[] newArr = sort.sort(arr);
+		long end = System.currentTimeMillis();
+		System.out.println("radixSort用时：" + (end - begin));
 		System.out.println(Arrays.toString(newArr));
 	}
 
